a6a6278d139183a57383f6b0abb92e42ab13e5d5
seang <sreangvengseang31@gmail.com>
2025-01-10 19:10:53 +0700

    Loop update

---
+++ loop/Ractangleloop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    for (int i=1 ; i <=10; i--)//row
+    {
+        for (int a = i; a <=5; a--)//column
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+}
+//i=0 ; i<=10 ;true;
+    //a=i ; a<=10 ;true;
+    //a=1; a<=10; true;
+    //a
