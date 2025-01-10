d11f60a1a27680339facc721cc4875dc392aef1d
seang <sreangvengseang31@gmail.com>
2025-01-10 18:37:52 +0700

    nestloop

---
+++ loop/Excercise1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    int number;
+    int count;
+    float average;
+
+    cout<<"sum of number input"<<endl;
+    cout<<"------------------------"<<endl;
+    cout<<"Oy ke tver man dong ng min dak mk             :";
+    cin>>count;
+    cout<<"------------------------"<<endl;
+
+    for (int i = 1; i <=count; i++)
+    {
+        cout<<"Min pjol mk cherm men                     :";
+        cin>>number;
+        sum = sum + number;
+    }
+    cout<<"Mer latphorl klun eng tv yii                  :"<<sum<<endl;
+    average = sum /count;
+    cout<<"Ot jes tver klun eng teh men lv smer pin ng   :"<<average;
+
+
+}
--- loop/Good.cpp
+++ loop/Good.cpp
@@ -1,5 +1,43 @@
+
+    // syntax :
+    //     for(start; condition; increament or decreameant){
+    //         statement code;
+    //     }
+// #include <iostream>
+// using namespace std;
+// int main(){
+  
+    // for(int i = 1; i<10; i=i+2){
+    //     cout<<i<<endl;
+    // }
+
+    
+    // for(int i = 1; i<=5; i++){
+    //     cout<<i*2<<endl;
+    // }
+    
+    // for(int j = 1; j<10; j++){
+    //     cout<<j*2 - 1<<endl;
+    // }
+
+// }
+
+//note :
+//i = 0; i<5; true ; hello
+//i = 1; i<5; true ; hello
+//i = 2; i<5; true ; hello
+//i = 3; i<5; true ; hell0
+//i = 4; i<5; false; no execute;
+
+
+// i = 1+2 = 3; true;
 #include <iostream>
 using namespace std;
 int main(){
+    
+    for (int i; i <10; i++)
+    {
+        cout<<"Hello World"<<i<<endl;
+    }
     
 }
